15d4aa203e14f5d88d13294ec439432b13070cff
brunovieira005 <brunorv2005@gmail.com>
2025-09-30 22:56:57 -0300

    Aula 30/09
    Filters e melhora nos serializers

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -5,27 +5,28 @@ from django.contrib.auth.admin import UserAdmin
 class AdminCustomUser(UserAdmin):
     model = CustomUser
     list_display = ['id', 'email', 'nif']
-    list_display_links = ('id', 'email', 'nif')
+    list_display_links = ('id', 'email', 'nif',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('User data', {'fields': ('nif', 'phone', 'creation_date')}),
+        (None, {'fields': ('email','password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 
+                                    'is_superuser', 'groups', 
+                                    'user_permissions',)}),
+        ('User data', {'fields': ('nif', 'phone',)}),
     )
-    filter_horizontal = ('groups', 'user_permissions')
+    filter_horizontal = ('groups', 'user_permissions',)
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'name', 'nif', 'password1', 'password2'),
+            'fields': ('email','name','nif','password1','password2'),
         }),
     )
     ordering = ['email']
 
 admin.site.register(CustomUser, AdminCustomUser)
-admin.site.register(Category)
 admin.site.register(Environment)
+admin.site.register(Category)
 admin.site.register(Equipment)
-admin.site.register(Notification)
 admin.site.register(Task)
 admin.site.register(TaskStatus)
 admin.site.register(TaskStatusImage)
-
+admin.site.register(Notification)
