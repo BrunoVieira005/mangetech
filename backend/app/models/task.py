5c383feb7f00e624ff8d29e7587573051d1c1d1d
brunovieira005 <brunorv2005@gmail.com>
2025-10-07 21:54:06 -0300

    Aula 07/10
    Criação do Serializer customizado e Serializers Read e Write para as Tasks
    
    Novos filters, relacionamento

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -6,13 +6,19 @@ class URGENCY_LEVELS(models.TextChoices):
     HIGH = 'HIGH', 'high'
     EXTRA_HIGH = 'EXTRA_HIGH', 'extra_high'
 
+
 class Task(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=1000)
     suggested_date = models.DateTimeField()
-    urgency_level = models.CharField(max_length=50, choices=URGENCY_LEVELS.choices, default= URGENCY_LEVELS.LOW)
+    urgency_level = models.CharField(max_length=50, 
+                                     choices=URGENCY_LEVELS.choices,
+                                     default=URGENCY_LEVELS.LOW)
     creation_date = models.DateTimeField(auto_now_add=True)
-    creator_FK = models.ForeignKey('CustomUser', related_name='Task_creator_FK', on_delete=models.SET_NULL, null=True)
+    creator_FK = models.ForeignKey('CustomUser', 
+                                related_name='Task_creator_FK',
+                                on_delete=models.SET_NULL,
+                                null=True)
     equipments_FK = models.ManyToManyField('Equipment')
     responsibles_FK = models.ManyToManyField('CustomUser')
 
